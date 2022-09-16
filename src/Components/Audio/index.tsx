506df6beb37bdd4245b0ee154d8a749d0eea9f6e
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-17 01:10:35 +0300

    1

---
+++ src/Components/Audio/index.tsx
@@ -0,0 +1,21 @@
+import boom  from './boom.wav';
+import clap from './clap.wav'
+import hihat from './hihat.wav'
+import kick from './kick.wav'
+import openhat from './openhat.wav'
+import ride from './ride.wav'
+import snare from './snare.wav'
+import tink from './tink.wav'
+import tom from './tom.wav'
+
+export const audio = {
+    boom: boom,
+    clap: clap,
+    hihat: hihat,
+    kick: kick,
+    openhat: openhat,
+    ride: ride,
+    snare: snare,
+    tink: tink,
+    tom: tom
+}
