011b5a26e4993600292067b56b8ad84935b40c53
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-17 00:15:37 +0300

    Early Hours

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import {Piano} from './Components/index'
+
+function App() {
+  return (
+    <div className="App">
+      <Piano />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Piano/Piano.tsx
@@ -0,0 +1,13 @@
+import Styles from './Piano.module.scss';
+
+export const Piano = () => {
+  return (
+      <div>
+          <div className={Styles.Top}>
+              
+          </div>
+    </div>
+  )
+}
+
+export default Piano;
